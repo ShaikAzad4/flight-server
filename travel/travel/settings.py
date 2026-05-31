5fe98df0219a6a2d8903c1739d712eef5b1d3065
ShaikAzad4 <shaikazad3131@gmail.com>
2026-05-31 15:14:59 +0530

    fixed django storages config

--- travel/travel/settings.py
+++ travel/travel/settings.py
@@ -148,7 +148,12 @@ AWS_SECRET_ACCESS_KEY = os.getenv("AWS_SECRET_ACCESS_KEY")
 AWS_STORAGE_BUCKET_NAME = "azad-django-flight-images"
 AWS_S3_REGION_NAME = "us-east-1"
 MEDIA_URL = f"https://{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com/"
-DEFAULT_FILE_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
+
+STORAGES = {
+    "default": {
+        "BACKEND": "storages.backends.s3boto3.S3Boto3Storage",
+    }
+}
 
 
 
